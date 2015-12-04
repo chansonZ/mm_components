2555509fc1fad608ab80e3ddba12f9d581e0a987
Samuel Lampa <samuel.lampa@gmail.com>
2015-12-04 14:20:30 +0100

    Re-order imports alphabetically

--- mmcomp/__init__.py
+++ mmcomp/__init__.py
@@ -2,7 +2,10 @@ from mmcomp import components
 from mmcomp.components import AssessLinearRMSD
 from mmcomp.components import AssessSVMRMSD
 from mmcomp.components import CalcAverageRMSDForCost
+from mmcomp.components import CollectDataReport
+from mmcomp.components import CountLines
 from mmcomp.components import CreateFolds
+from mmcomp.components import CreateRandomData
 from mmcomp.components import CreateSparseTestDataset
 from mmcomp.components import CreateSparseTrainDataset
 from mmcomp.components import CreateRunCopy
@@ -15,11 +18,7 @@ from mmcomp.components import PredictSVMModel
 from mmcomp.components import SampleTrainAndTest
 from mmcomp.components import SelectLowestRMSD
 from mmcomp.components import SelectPercentIndexValue
+from mmcomp.components import ShuffleLines
 from mmcomp.components import TrainLinearModel
 from mmcomp.components import TrainSVMModel
 from mmcomp.components import UnGzipFile
-from mmcomp.components import CountLines
-from mmcomp.components import CreateRandomData
-from mmcomp.components import ShuffleLines
-from mmcomp.components import CollectDataReportRow
-from mmcomp.components import CollectDataReport
